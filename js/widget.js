70d0fc131dac8f15c81ddad2fb2c83720a392f7e
Dou Du <dou.du@icloud.com>
2024-02-15 09:59:25 +0100

    make it work for the 1st step

--- js/widget.js
+++ js/widget.js
@@ -1,7 +1,9 @@
 import "./widget.css";
+import { basicSetup, EditorView } from 'codemirror'
+import { javascript } from '@codemirror/language'
 
 function render({ model, el }) {
-	let btn = document.createElement("button");
+	/* let btn = document.createElement("button");
 	btn.classList.add("anywidget_code_input-counter-button");
 	btn.innerHTML = `count is ${model.get("value")}`;
 	btn.addEventListener("click", () => {
@@ -11,7 +13,20 @@ function render({ model, el }) {
 	model.on("change:value", () => {
 		btn.innerHTML = `count is ${model.get("value")}`;
 	});
-	el.appendChild(btn);
+	el.appendChild(btn); */
+
+    let textarea = document.createElement("div");
+    const initialText = 'console.log("hello, world")'
+
+    let cm = new EditorView({
+        doc: initialText,
+        extensions: [
+            basicSetup,
+        ],
+        parent: textarea,
+    });
+
+	el.appendChild(textarea);
 }
 
 export default { render };
